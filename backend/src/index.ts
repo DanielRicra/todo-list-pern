02ea86884febddcf04f8a2611093a3d4de8a6ac4
DanielRicra <daniel.int@outlook.com>
2023-05-28 14:49:29 -0500

    Add cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import taskRoutes from './routes/taskRoutes';
 import taskListRoutes from './routes/taskListRoutes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/v1/task', taskRoutes);
 app.use('/api/v1/taskList', taskListRoutes);
