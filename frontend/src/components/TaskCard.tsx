21067fcf286006762ab96974186b3c44b12794a1
DanielRicra <daniel.int@outlook.com>
2023-05-29 23:26:14 -0500

    Add new TaskCard, with the old name of TaskCardContent
    - The old TaskCard passed to be TaskCardContent.
    - This new TaskCard handles the functionlity to close the dropdown
      when the mouse is clicked outside the dropdow menu.
    - Returns a TaskCardComponent passing the task, ref, thres props to
      toggle the dropdow.

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -1,92 +1,38 @@
-import { useEffect, useState } from 'react';
-import CaretRight from './icons/CaretRight';
-import Dots from './icons/Dots';
-import { Task, TaskProps } from '../types';
-import { useAppDispatch } from '../app/hooks';
-import { updateTask } from '../features/task/tasksSlice';
-import { convertTaskDTOToTask, convertTaskToTaskDTO } from '../utils/taskUtils';
+import { useState, useRef, useEffect } from 'react';
+import { TaskDTO } from '../types';
+import TaskCardContent from './TaskCardContent';
 
-const TaskCard = ({ task }: TaskProps) => {
-	const dispatch = useAppDispatch();
-	const [taskForm, setTaskForm] = useState<Task>(convertTaskDTOToTask(task));
+const TaskCard = ({ task }: { task: TaskDTO }) => {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-	const handleCompletedToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const completedAt = e.target.checked ? new Date().toISOString() : undefined;
-		dispatch(updateTask({ ...convertTaskToTaskDTO(taskForm), completedAt }));
-	};
+	const ref = useRef<HTMLDivElement>(null);
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target; 
-		dispatch(updateTask({ ...convertTaskToTaskDTO(taskForm), [name]: value }));
+	const toggleShowDropdown = () => {
+		setIsDropdownOpen((p) => !p);
 	};
 
 	useEffect(() => {
-		setTaskForm(convertTaskDTOToTask(task));
-	}, [task]);
+		const handleClickOutside = (event: MouseEvent) => {
+			if (ref.current) {
+				if (!ref.current.contains(event.target as Node)) {
+					setIsDropdownOpen(false);
+				}
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+
+      return () => {
+         document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [ref]);
 
 	return (
-		<div className="flex w-full hover:bg-[#252531] rounded-lg p-2 flex-col justify-start items-start relative">
-			{task.description && (
-				<>
-					<input
-						type="checkbox"
-						id={`accordion-caret-${task.id}`}
-						className="cursor-pointer absolute opacity-0 peer/accordion -z-10"
-						title="Done"
-					/>
-					<label
-						htmlFor={`accordion-caret-${task.id}`}
-						className="absolute left-[-10px] z-10 peer-checked/accordion:rotate-90 transition-all duration-200 ease-linear"
-					>
-						<CaretRight
-							fill="none"
-							className="h-6 w-6 flex"
-							strokeWidth="2"
-						/>
-					</label>
-				</>
-			)}
-
-			<div className="flex items-center justify-between w-full gap-2 mb-2">
-				<label
-					htmlFor={`input-done-${task.id}`}
-					className="ml-2 cursor-pointer relative flex items-center text-base"
-				>
-					<input
-						type="checkbox"
-						id={`input-done-${task.id}`}
-						className="cursor-pointer absolute opacity-0 peer/completed -z-10"
-						title="Completed"
-						name="completed"
-						onChange={handleCompletedToggle}
-						checked={!!taskForm.completedAt}
-					/>
-					<span className="border-2 border-[#F84C6F] border-solid peer-checked/completed:text-white bg-transparent peer-checked/completed:bg-[#F84C6F] peer-checked/completed:bg-[url('src/assets/check.svg')] peer-checked/completed:bg-no-repeat peer-checked/completed:bg-center block rounded-lg -top-[2px] w-[22px] h-[22px] pointer-events-none transition-all duration-300 ease-in-out"></span>
-				</label>
-
-				<input
-					type="text"
-					name="title"
-					value={taskForm.title}
-					placeholder="Write a task"
-					className="flex-1 bg-transparent outline-none"
-					onChange={handleInputChange}
-				/>
-
-				<Dots fill="#f5f2f2" />
-			</div>
-			{task.description && (
-				<p className="h-0 px-2 py-0 ml-7 overflow-hidden transition-all duration-200 ease-linear peer-checked/accordion:h-auto peer-checked/accordion:p-1 peer-checked/accordion:overflow-visible">
-					{task.description}
-				</p>
-			)}
-			{taskForm.dueDate && (
-				<span className="ml-7">{taskForm.dueDate.toLocaleDateString()}</span>
-			)}
-			{taskForm.updatedAt && (
-				<span className="ml-7">{taskForm.updatedAt.toLocaleDateString()}</span>
-			)}
-		</div>
+		<TaskCardContent
+			task={task}
+			ref={ref}
+			toggleShowDropdown={toggleShowDropdown}
+			isDropdownOpen={isDropdownOpen}
+		/>
 	);
 };
 
