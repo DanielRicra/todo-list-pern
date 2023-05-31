3678f454f6e049487f2b35bffd34095125599e4f
DanielRicra <daniel.int@outlook.com>
2023-05-30 20:41:05 -0500

    Add dispatch taskLists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,28 +5,20 @@ import { useAppDispatch } from './app/hooks';
 import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
 import TodoList from './components/TodoList';
-import { setTasks } from './features/task/tasksSlice';
-import { tasks, taskLists } from './db';
-import { setTaskLists } from './features/task-list/taskListSlice';
+import { fetchTaskListsByUserId } from './features/task-list/taskListMiddleware';
 
 function App() {
 	const dispatch = useAppDispatch();
 
 	useEffect(() => {
-		dispatch(setTaskLists(taskLists));
-
-		dispatch(
-			setTasks({
-				tasks: tasks.filter((task) => task.taskListId === taskLists[0].taskListId),
-				taskListId: taskLists[0].taskListId,
-			})
-		);
-	}, [dispatch]);
+		dispatch(fetchTaskListsByUserId(1));
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<>
 			<div className="min-h-screen w-[260px]">
-				<SideBar />
+				<SideBar  />
 			</div>
 
 			<div className="min-h-screen flex-1">
