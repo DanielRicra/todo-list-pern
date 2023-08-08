3cfad06851bebcc7bb94693cc09de4e5bb3e7edd
DanielRicra <daniel.int@outlook.com>
2023-08-08 10:16:48 -0500

    Update tor add getUsers functions and remove login

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,37 +1,8 @@
-import { PrismaClient } from '@prisma/client';
 import type { Request, Response } from 'express';
+
 import { HTTP_STATUS } from '../utils/constants';
+import prisma from '../libs/prisma';
 
-const prisma = new PrismaClient();
-
-export const login = async (req: Request, res: Response): Promise<void> => {
-	try {
-		const { email, password } = req.body;
-
-		const existingUser = await prisma.user.findFirst({
-			where: {
-				email,
-			},
-		});
-
-		if (!existingUser) {
-			res.status(HTTP_STATUS.NOT_FOUND).json({ error: 'User not found' });
-			return;
-		}
-
-		if (password !== existingUser.password) {
-			res.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'Invalid credentials' });
-         return;
-		}
-
-		res
-			.status(200)
-			.json({
-				email: existingUser.email,
-				userId: existingUser.userId,
-				name: existingUser.name,
-			});
-	} catch (error) {
-		res.status(500).json({ error: 'Something went wrong' });
-	}
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+	res.status(HTTP_STATUS.OK).json(await prisma.user.findMany());
 };
