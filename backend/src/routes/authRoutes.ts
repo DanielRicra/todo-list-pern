1e52b9f2636727da6e13e08c64908337a63adf2f
DanielRicra <daniel.int@outlook.com>
2023-08-09 16:47:51 -0500

    Update authRoutes
    - Add POST /token route, to revalidate access token.
    -Add DELETE /token route to logout the user.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { login, signUp } from '../controllers/authController';
+import {
+	login,
+	logout,
+	revalidateAccessToken,
+	signUp,
+} from '../controllers/authController';
 
 const authRoutes = Router();
 
 authRoutes.post('/login', login);
 authRoutes.post('/signup', signUp);
+authRoutes.post('/token', revalidateAccessToken);
+authRoutes.delete('/token', logout);
 
 export default authRoutes;
