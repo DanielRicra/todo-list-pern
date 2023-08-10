07e89f3abe2183df4b09f0edc2b8e7e2a63075c2
DanielRicra <daniel.int@outlook.com>
2023-08-09 22:53:13 -0500

    Update userController to add more functions
    - Add getUserById. Returns the user including roles tasksLists, tasks.
    
    - Add handle error for getUSers, and include roles.

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,8 +1,47 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 import { HTTP_STATUS } from '../utils/constants';
 import prisma from '../libs/prisma';
 
-export const getUsers = async (req: Request, res: Response): Promise<void> => {
-	res.status(HTTP_STATUS.OK).json(await prisma.user.findMany());
+export const getUsers = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> => {
+	try {
+		const users = await prisma.user.findMany({ include: { roles: true } });
+		res.status(HTTP_STATUS.OK).json(users);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getUserById = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> => {
+	try {
+		const { userId } = req.params;
+		const user = await prisma.user.findUniqueOrThrow({
+			where: { userId: Number(userId) },
+			include: {
+				roles: true,
+				taskLists: {
+					include: {
+						tasks: true,
+					},
+				},
+			},
+		});
+		res.status(HTTP_STATUS.OK).json(user);
+	} catch (error) {
+		if (error instanceof PrismaClientKnownRequestError) {
+			if (error.code === 'P2025') {
+				res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'User not found' });
+			}
+		}
+		next(error);
+	}
 };
