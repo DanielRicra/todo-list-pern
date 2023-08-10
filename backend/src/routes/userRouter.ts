e700c4b99bc8a861789171c336cbc1051cb38f4c
DanielRicra <daniel.int@outlook.com>
2023-08-09 22:49:42 -0500

    Add middleware and route
    - Route /:userId to ge a user by id.
    - Add very token middleware ad for / rote also add verifyAdmin.

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getUsers } from '../controllers/userController';
+import { getUserById, getUsers } from '../controllers/userController';
+import { verifyAdmin, verifyToken } from '../middleware/authJWT';
 
 const userRoutes = Router();
 
-userRoutes.get('/', getUsers);
+userRoutes.get('/', [verifyToken, verifyAdmin], getUsers);
+userRoutes.get('/:userId', verifyToken, getUserById);
 
 export default userRoutes;
