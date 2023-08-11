d24364ad80f690a182255278aacd943c30b79298
DanielRicra <daniel.int@outlook.com>
2023-08-11 15:17:54 -0500

    Add default workspace to show in todos

---
+++ frontend/src/pages/workspace/workspace.tsx
@@ -0,0 +1,15 @@
+const Workspace = () => {
+	return (
+		<>
+			<div>
+				<h1>Hello start creating a new Task List</h1>
+
+				<p>
+					Some of them want to use you, some of them want to get used by
+					you!
+				</p>
+			</div>
+		</>
+	);
+};
+export default Workspace;
