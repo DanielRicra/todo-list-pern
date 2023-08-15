926fb18917ade4987cfdcf9a93607b6fb11b5b24
DanielRicra <daniel.int@outlook.com>
2023-08-14 22:23:24 -0500

    Update app.tsx to add ThemeProvider

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
+import { ThemeProvider } from './providers/theme-provider';
 
 function App() {
-	return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+	return (
+		<ThemeProvider>
+			<RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+		</ThemeProvider>
+	);
 }
 
 export default App;
