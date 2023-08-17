3d24dff57a3ca005779e000f8fce754401886fbf
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:37:50 -0500

    Add dark mode toggle component to navbar

--- frontend/src/components/nav-bar.tsx
+++ frontend/src/components/nav-bar.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from './mode-toggle';
 import SearchTask from './search-task';
 
 const NavBar = () => {
@@ -5,6 +6,7 @@ const NavBar = () => {
 		<nav className="flex items-center bg-[#21212b] justify-end px-6 py-2 gap-2 shadow-lg z-10 sticky">
 			<SearchTask />
 			<div>🔔</div>
+			<ModeToggle />
 			<div className="avatar w-8 h-8 rounded-full bg-green-500 flex items-center justify-center">
 				<span className="text-white font-medium text-base">DR</span>
 			</div>
