04e67b6e06dbe6b85300bb6036db7b06aede0de8
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:23:19 -0500

    Complete login-form to on submit function

--- frontend/src/pages/auth/components/login-form.tsx
+++ frontend/src/pages/auth/components/login-form.tsx
@@ -1,6 +1,8 @@
-import { email, minLength, object, string, type Output, } from 'valibot';
+import { email, minLength, object, string, type Output } from 'valibot';
 import { useForm } from 'react-hook-form';
 import { valibotResolver } from '@hookform/resolvers/valibot';
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -12,6 +14,10 @@ import {
 	FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { login } from '@/services/authService';
+import { useToast } from '@/components/ui/use-toast';
+import { User } from '@/types';
+import { useUserActions } from '@/hooks/useUserActions';
 
 const formSchema = object({
 	email: string([
@@ -19,15 +25,18 @@ const formSchema = object({
 		email('The email address is badly formatted.'),
 	]),
 	password: string([
-		minLength(8, 'Your password must have 8 characters or more.'),
+		minLength(1, 'Your password is required.'),
 	]),
 });
 
 type FormData = Output<typeof formSchema>;
 
 const LoginForm = () => {
+	const { toast } = useToast();
+	const { saveUser } = useUserActions();
+	const navigate = useNavigate();
 
-   const form = useForm<FormData>({
+	const form = useForm<FormData>({
 		resolver: valibotResolver(formSchema),
 		defaultValues: {
 			email: '',
@@ -35,8 +44,23 @@ const LoginForm = () => {
 		},
 	});
 
-	const onSubmit = (values: FormData) => {
-		console.log(values);
+	const onSubmit = async (values: FormData) => {
+		try {
+			const authData = await login(values.email, values.password);
+			const decoded = jwtDecode<User>(authData.accessToken);
+			saveUser({
+				...decoded,
+				accessToken: authData.accessToken,
+				refreshToken: authData.refreshToken,
+			});
+			navigate('/workspace');
+		} catch (error) {
+			toast({
+				title: 'Error',
+				description: (error as Error).message,
+				variant: 'destructive',
+			});
+		}
 	};
 
 	return (
