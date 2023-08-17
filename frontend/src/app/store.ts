62aaef6e914f6395ce6e19e95772c5acccd4e21d
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:28:23 -0500

    Update store.ts
    - Remove tasks and taskList reducer, to use RTK query instead
    
    - taskListApi to reducers, and its middleware.
    
    - add local storage persistance middleware

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,14 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from '../features/task/taskSlice';
-import taskListReducer from '../features/task-list/taskListSlice';
+import { type Middleware, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+
+import userReducer from '../features/user/userSlice';
+import { taskListApi } from '@/services/task-list';
+import { STORAGE_STORE_KEY } from '@/constants';
+
+const persistanceLocalStorageMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		next(action);
+		localStorage.setItem(STORAGE_STORE_KEY, JSON.stringify(store.getState()));
+	};
 
 export const store = configureStore({
 	reducer: {
-      tasks: tasksReducer,
-      taskList: taskListReducer
-   },
-	devTools: true,
+		user: userReducer,
+		[taskListApi.reducerPath]: taskListApi.reducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware()
+			.concat(taskListApi.middleware)
+			.concat(persistanceLocalStorageMiddleware),
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
