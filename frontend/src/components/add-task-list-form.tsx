cca570677bfd975e5e591458b872bd93587fb6a6
DanielRicra <daniel.int@outlook.com>
2023-08-17 10:04:06 -0500

    Update add-task-list-form
    - Replace thunk middleware to use `useAddTaskListMutation`
    
    - Use Form component nad valibot validation.

--- frontend/src/components/add-task-list-form.tsx
+++ frontend/src/components/add-task-list-form.tsx
@@ -1,72 +1,77 @@
-import { useRef } from 'react';
-import { useAppDispatch } from '../app/hooks';
-import type { TaskList } from '../types';
-import AddButton from './buttons/AddButton';
-import { saveTaskList } from '../features/task-list/taskListMiddleware';
+import { useForm } from 'react-hook-form';
+import { minLength, string, type Output, object } from 'valibot';
+
+import { useAddTaskListMutation } from '@/services/task-list';
+import { useAppSelector } from '@/app/hooks';
+import { selectUser } from '@/features/user/userSlice';
+import { valibotResolver } from '@hookform/resolvers/valibot';
+import { Form, FormControl, FormField, FormItem, FormMessage } from './ui/form';
+import { useToast } from './ui/use-toast';
+import { Button } from './ui/button';
+import { PlusIcon } from '@radix-ui/react-icons';
+
+const formSchema = object({
+	name: string([minLength(1, 'Please enter a name')]),
+});
+
+type FormData = Output<typeof formSchema>;
 
 const AddTaskListForm = () => {
-	const dispatch = useAppDispatch();
-	const inputRef = useRef<HTMLInputElement>(null);
-	const errorRef = useRef<HTMLParagraphElement>(null);
+	const { userId } = useAppSelector(selectUser);
+	const [addTaskList, { isLoading: isAdding }] = useAddTaskListMutation();
+	const { toast } = useToast();
 
-	const addNewTaskList = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
+	const form = useForm<FormData>({
+		resolver: valibotResolver(formSchema),
+		defaultValues: {
+			name: '',
+		},
+	});
 
-		if (inputRef.current?.value.trim() === '') {
-			if (errorRef.current) {
-				errorRef.current.style.display = 'block';
-			}
-
-			setTimeout(() => {
-				if (errorRef.current) {
-					errorRef.current.style.display = 'none';
-				}
-			}, 3000);
-			return;
+	const onSubmit = async (values: FormData) => {
+		try {
+			await addTaskList({ name: values.name, userId }).unwrap();
+		} catch (error) {
+			toast({
+				title: 'Error',
+				description: (error as Error).message,
+				variant: 'destructive',
+			});
 		}
-
-		const newTaskList: TaskList = {
-			name: inputRef.current?.value ?? 'New TaskList',
-			userId: 1,
-		};
-
-		if (errorRef.current) {
-			errorRef.current.style.display = 'none';
-		}
-
-		if (inputRef.current) {
-			inputRef.current.value = '';
-		}
-
-		dispatch(saveTaskList(newTaskList));
 	};
 
 	return (
-		<form
-			onSubmit={addNewTaskList}
-			className="rounded-2xl p-2 flex border border-[#3e3e4b] w-full mt-3 shadow-md relative mb-3"
-		>
-			<AddButton
-				title="Ass New TaskList"
-				type="submit"
-				bgColor="#AA28A3"
-				size={18}
-			/>
-			<input
-				ref={inputRef}
-				type="text"
-				placeholder="Add Task List..."
-				className="bg-transparent border border-none text-white w-full px-2 outline-none rounded-2xl"
-			/>
-
-			<p
-				style={{ display: 'none' }}
-				ref={errorRef}
-				className="text-[#f84c4c] text-sm absolute bottom-0 right-2 translate-y-full"
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="rounded-2xl p-2 flex items-center border border-[#3e3e4b] w-full mt-3 shadow-md relative mb-3"
 			>
-				Please fill the field
-			</p>
-		</form>
+				<Button
+					type="submit"
+					disabled={isAdding}
+					className="rounded-lg aspect-square p-0 h-7 w-7"
+				>
+					<PlusIcon className="h-5 w-5" />
+				</Button>
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem className="w-full space-y-0">
+							<FormControl className="flex-1">
+								<input
+									placeholder="Task list name"
+									className="bg-transparent border-none text-white w-full px-2 outline-none rounded-2xl active:outline-none"
+									disabled={isAdding}
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage className="text-[#f84c4c] text-sm px-2" />
+						</FormItem>
+					)}
+				/>
+			</form>
+		</Form>
 	);
 };
 
