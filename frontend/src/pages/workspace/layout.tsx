d5283ff9a498f0e7c98e47356ea240ca956c7c21
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:45:11 -0500

    Update workspace page layout
    - Remove `fetchTaskListsByUserId` with dispatch from the WorkSpaceLayout

--- frontend/src/pages/workspace/layout.tsx
+++ frontend/src/pages/workspace/layout.tsx
@@ -1,18 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import NavBar from '@/components/nav-bar';
 import SideBar from '@/components/side-bar';
-import { useAppDispatch } from '@/app/hooks';
-import { fetchTaskListsByUserId } from '@/features/task-list/taskListMiddleware';
 
 const WorkspaceLayout = () => {
-	const dispatch = useAppDispatch();
-
-	useEffect(() => {
-		dispatch(fetchTaskListsByUserId(1));
-	}, []);
-
 	return (
 		<>
 			<div className="min-h-screen w-[260px]">
