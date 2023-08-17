2e255bb36a6d6d180453f2badbf65f398dda6945
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:23:50 -0500

    Update Workspace to add some styles

--- frontend/src/pages/workspace/workspace.tsx
+++ frontend/src/pages/workspace/workspace.tsx
@@ -1,8 +1,8 @@
 const Workspace = () => {
 	return (
 		<>
-			<div>
-				<h1>Hello start creating a new Task List</h1>
+			<div className="p-4 lg:p-6 flex justify-start items-center flex-col">
+				<h1 className='text-2xl text-center my-3'>Hello start creating a new Task List</h1>
 
 				<p>
 					Some of them want to use you, some of them want to get used by
