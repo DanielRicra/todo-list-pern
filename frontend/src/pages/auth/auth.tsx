b8dc204ca00204c1e89cbb7bc22722e914f8ea67
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:25:06 -0500

    Update auth.tsx to add Toaster

--- frontend/src/pages/auth/auth.tsx
+++ frontend/src/pages/auth/auth.tsx
@@ -1,29 +1,34 @@
 import { Link, useLocation } from 'react-router-dom';
 import LoginForm from './components/login-form';
 import SignUpForm from './components/sign-up-form';
+import { Toaster } from '@/components/ui/toaster';
 
 const AuthPage = () => {
 	const params = useLocation();
 	const isLogin = params.pathname.includes('login');
 
 	return (
-		<div className="min-h-screen w-full flex justify-center items-center flex-col bg-[#181820] text-white">
-			<h1 className="text-3xl font-medium mb-5">
-				{isLogin ? 'Login' : 'Sign Up'}
-			</h1>
+		<>
+			<div className="min-h-screen w-full flex justify-center items-center flex-col bg-[#181820] text-white">
+				<h1 className="text-3xl font-medium mb-5">
+					{isLogin ? 'Login' : 'Sign Up'}
+				</h1>
 
-			{isLogin ? <LoginForm /> : <SignUpForm />}
+				{isLogin ? <LoginForm /> : <SignUpForm />}
 
-			<div className="text-gray-300 mt-2">
-				{isLogin ? 'No account?' : 'Already have an account?'}
-				<Link
-					to={isLogin ? '/signup' : '/login'}
-					className="ml-2 underline hover:opacity-80"
-				>
-					{isLogin ? 'Sign Up' : 'Login'}
-				</Link>
+				<div className="text-gray-300 mt-2">
+					{isLogin ? 'No account?' : 'Already have an account?'}
+					<Link
+						to={isLogin ? '/signup' : '/login'}
+						className="ml-2 underline hover:opacity-80"
+					>
+						{isLogin ? 'Sign Up' : 'Login'}
+					</Link>
+				</div>
 			</div>
-		</div>
+
+			<Toaster />
+		</>
 	);
 };
 
