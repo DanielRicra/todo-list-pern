c674d86e635572ef40b2ff0b9a1253a2b931f357
DanielRicra <daniel.int@outlook.com>
2023-08-16 23:52:40 -0500

    Update todo-list to use taskListApi (RTK query)
    - Now the tasks comes from `useGetTaskListByIdQuery`.

--- frontend/src/components/todo-list.tsx
+++ frontend/src/components/todo-list.tsx
@@ -1,36 +1,43 @@
 import { useState, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Eye from './icons/Eye';
 import EyeClosed from './icons/EyeClosed';
-import { useAppSelector } from '../app/hooks';
 import AddTaskForm from './add-task-form';
 import TaskCard from './task-card/task-card';
+import { useGetTaskListByIdQuery } from '@/services/task-list';
+import { TaskDTO } from '@/types';
 
 export const TodoList = () => {
-	const [showCompleted, setShowCompleted] = useState(false);
+	const { taskListId } = useParams();
+
 	const {
-		value: { name: TaskListName, tasks },
-		status,
-		error,
-	} = useAppSelector((state) => state.tasks);
+		data: taskList,
+		isLoading,
+		isError,
+	} = useGetTaskListByIdQuery(taskListId ?? '');
+
+	const [showCompleted, setShowCompleted] = useState(false);
 
 	const filteredTasks = useMemo(() => {
-		return showCompleted ? tasks : tasks.filter((task) => !task.completedAt);
-	}, [showCompleted, tasks]);
+		return showCompleted ? taskList?.tasks : taskList?.tasks.filter((task: TaskDTO) => !task.completedAt);
+	}, [showCompleted, taskList]);
 
 	const toggleShowCompleted = () => {
 		setShowCompleted((prev) => !prev);
 	};
 
-	const completedTasksCount = tasks.filter((t) =>
+	const completedTasksCount = taskList?.tasks.filter((t: TaskDTO) =>
 		Boolean(t.completedAt)
 	).length;
 
 	return (
-		<div className="bg-[#181820] h-full p-4 flex place-content-center">
+		<div className="bg-[#181820] h-full p-4 flex place-content-center text-white">
 			<div className="flex flex-col items-start justify-start sm:max-w-[400px] lg:max-w-[600px] w-full py-8">
 				<div className="flex items-center justify-between w-full">
-					<h2 className="font-semibold text-2xl">{TaskListName}</h2>
+					<h2 className="font-semibold text-2xl">
+						{taskList?.name}- {taskListId}
+					</h2>
 					<div
 						className="dots-icon cursor-pointer hover:bg-[#252531] rounded-md p-1 relative inline-block group"
 						onClick={toggleShowCompleted}
@@ -48,25 +55,22 @@ export const TodoList = () => {
 				<AddTaskForm />
 
 				<div className="flex gap-4 items-center w-full mt-3 mb-2">
-					<h3 className="">Tasks - {tasks.length}</h3>
+					<h3 className="">Tasks - {taskList?.tasks.length}</h3>
 					<div className="h-[8px] bg-[#34343f] rounded-md flex-1">
 						<div
 							className="h-full bg-[#AA28A3] rounded-md"
 							style={{
-								width: `${(completedTasksCount / tasks.length) * 100}%`,
+								width: `${(completedTasksCount / taskList?.tasks.length) * 100}%`,
 							}}
 						/>
 					</div>
 				</div>
 				<div className="flex flex-col items-start justify-start w-full gap-2">
-					{status === 'pending' && (
+					{isLoading ? (
 						<div className="animate-pulse text-2xl">Loading</div>
-					)}
-					{status === 'rejected' && (
-						<div className="text-2xl text-red-500">{error}</div>
-					)}
-					{status === 'fulfilled' &&
-						filteredTasks.map((task) => (
+					) : isError ? (
+						<div className="text-2xl text-red-500">Error Loading Tasks Data...</div>
+					) : filteredTasks?.map((task: TaskDTO) => (
 							<TaskCard key={task.taskId} task={task} />
 						))}
 				</div>
@@ -74,5 +78,3 @@ export const TodoList = () => {
 		</div>
 	);
 };
-
-export default TodoList;
